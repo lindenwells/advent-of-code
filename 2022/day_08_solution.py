6262d06b593d6a2fb9792907d1a9416ac95c21ee
Linden Wells <lindenwells9@gmail.com>
2022-12-10 13:40:36 +1000

    day 8 part b

--- 2022/day_08_solution.py
+++ 2022/day_08_solution.py
@@ -39,32 +39,49 @@ print(
 # endregion
 
 # region part b
+def sight(trees: list[int], viewing_distance: int, height: int) -> int:
+    if trees == []:
+        return viewing_distance
+    else:
+        higher_than_next_tree = height > trees[0]
+        if not higher_than_next_tree:
+            return viewing_distance + 1
+        else:
+            return sight(trees[1:], viewing_distance + 1, height)
+
+
 def scenic_score(grid: list[list[int]], row: int, column: int) -> int:
 
-    value = grid[row][column]
+    height = grid[row][column]
     left, right, up, down = 0, 0, 0, 0
     if row in {0, len(grid) - 1} or column in {0, len(grid[0]) - 1}:
         # it's on the edge, so the product will be zero
         return 0
 
-    if value > max(grid[row][:column] or [-99]) or value > max(
-        grid[row][column + 1 :] or [-99]
-    ):
-        return True
+    left = sight(
+        trees=list(reversed(grid[row][:column])), viewing_distance=0, height=height
+    )
+    right = sight(
+        trees=list(grid[row][column + 1 :]), viewing_distance=0, height=height
+    )
+    up = sight(
+        trees=[row[column] for row in reversed(grid[:row])],
+        viewing_distance=0,
+        height=height,
+    )
+    down = sight(
+        trees=[row[column] for row in grid[row + 1 :]],
+        viewing_distance=0,
+        height=height,
+    )
 
-    transposed_grid = [list(sublist) for sublist in list(zip(*grid))]
-    if value > max(transposed_grid[column][:row] or [-99]) or value > max(
-        transposed_grid[column][row + 1 :] or [-99]
-    ):
-        return True
-
-    return False
+    return left * right * up * down
 
 
 print(
-    sum(
+    max(
         [
-            int(is_visible(grid, row, column))
+            scenic_score(grid, row, column)
             for row in range(len(grid))
             for column in range(len(grid[0]))
         ]
