88fec6880d9f5b8505a25f4bd455ddf76a1711c8
Linden Wells <62700647+lindenwells@users.noreply.github.com>
2022-12-13 17:16:35 +1000

    day 13 wip

--- 2022/day_13_solution.py
+++ 2022/day_13_solution.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from itertools import repeat
 import json
 from pprint import pprint
 
@@ -16,11 +18,63 @@ pairs = list(
 )
 
 
+def zip_longest(*args, fillvalue=None):
+    # zip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
+    iterators = [iter(it) for it in args]
+    num_active = len(iterators)
+    if not num_active:
+        return
+    while True:
+        values = []
+        for i, it in enumerate(iterators):
+            try:
+                value = next(it)
+            except StopIteration:
+                num_active -= 1
+                if not num_active:
+                    return
+                if i == 0:
+                    iterators[i] = repeat(fillvalue)
+                    value = fillvalue
+            values.append(value)
+        yield tuple(values)
+
+
 def ordered(x, y):
     if isinstance(x, int) and isinstance(y, int):
         return x < y
     elif isinstance(x, list) and isinstance(y, list):
-        return any([ordered(a, b) for a, b in zip(x, y)])
+        # if len(x) > 0:
+        #     if len(y) > 0:
+        #         return reduce(lambda a, b: a and ordered(b[0], b[1]), zip(x, y), True)
+        #     else:
+        #         return False
+        # else:
+        #     if len(y) > 0:
+        #         return True
+        #     else:
+        #         return True  # ?
+        # for index, value in enumerate(x):
+        # if index > len(y):
+
+        if x == [] and y != []:
+            return True
+        elif y == [] and x != []:
+            return False
+        else:
+            if isinstance(x[0], int) and isinstance(y[0], int):
+                return x < y
+            elif isinstance(x[0], int) and isinstance(y[0], list):
+                return ordered([x[0]], y[0])
+            elif isinstance(x[0], list) and isinstance(y[0], int):
+                return ordered(x[0], [y[0]])
+            elif isinstance(x[0], list) and isinstance(y[0], list):
+                return ordered(x[0], y[0])
+            elif x[0] == y[0]:
+                return ordered(x[1:], y[1:])
+            else:
+                return False
+
     elif isinstance(x, int) and isinstance(y, list):
         return ordered([x], y)
     elif isinstance(x, list) and isinstance(y, int):
@@ -28,4 +82,3 @@ def ordered(x, y):
 
 
 print([ordered(first, second) for index, (first, second) in enumerate(pairs)])
-print(len(list(pairs)))
