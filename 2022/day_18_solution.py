fc3d6d7182d460f6f8f1a76e5caaf88a83eac7e7
Linden Wells <lindenwells9@gmail.com>
2022-12-19 13:14:53 +1030

    I have been working on day 18 part 2 for 8 hours this is getting ridiculous this is probably my final commit for 2022

--- 2022/day_18_solution.py
+++ 2022/day_18_solution.py
@@ -1,5 +1,7 @@
-from functools import reduce
+from itertools import combinations
+from pprint import pprint
 
+points = set[tuple[int, int, int]]
 cubes = set(
     map(
         lambda line: tuple(
@@ -8,7 +10,7 @@ cubes = set(
                 line.strip().split(","),
             )
         ),
-        open("day_18_input.txt", "r").readlines(),
+        open("2022/day_18_input.txt", "r").readlines(),
     )
 )
 
@@ -45,14 +47,36 @@ air_droplets = {
 
 
 def air_pockets(rocks: set[tuple[int, int, int]]) -> set[tuple[int, int, int]]:
+    max_x, max_y, max_z = tuple(
+        max([coord[i] for coord in cubes]) + 1 for i in [0, 1, 2]
+    )
     possibilities = {
         (x, y, z)
-        for x in range(max([coord[0] for coord in cubes]))
-        for y in range(max([coord[1] for coord in cubes]))
-        for z in range(max([coord[2] for coord in cubes]))
+        for x in range(max_x + 1)
+        for y in range(max_y + 1)
+        for z in range(max_z + 1)
     }
-    start = (0, 0, 0)
-    return set()  # todo finish
+    pockets = set(map(lambda t: frozenset({t}), possibilities - rocks))
+    final_pocket = {(max_x, max_y, max_z)}
+    merges = 1
+    while merges > 0:
+        merges = 0
+        for pocket in pockets:
+            if neighbours(final_pocket, pocket):
+                final_pocket |= pocket
+                pockets -= pocket
+                merges += 1
+
+    pprint(final_pocket)
+    return final_pocket
+
+
+def neighbours(p1: points, p2: points) -> bool:
+    # two sets of points are neighbours if any neighbour of any point in p1 is the same as any point in p2
+    r = set()
+    for point in p1:
+        r |= neighbour_cubes(point)
+    return bool(r & p2)
 
 
 def part_b_surface_area(
@@ -62,4 +86,5 @@ def part_b_surface_area(
     return 6 - len((cubes | air_droplets) & neighbours)
 
 
-print(sum([part_b_surface_area(cube, cubes) for cube in cubes]))
+# print(sum([part_b_surface_area(cube, cubes) for cube in cubes]))
+air_pockets(cubes)
